5fd467528a09c3ff7bcb9850f1ccb884c0a45cac
mani.mallula <mani.mallula@gmail.com>
2022-11-21 12:08:45 +0530

    added

---
+++ service/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render,HttpResponse,HttpResponseRedirect
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import SignUpForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def base(request):
+    return render(request,'base.html')
+
+def home(request):
+    return render(request,'home.html')
+
+def booking(request):
+    return render(request,'booking.html')
+
+def tracking(request):
+    return render(request,'tracking.html')
+
+def profile(request):
+    return render(request,'profiles.html')
+
+def sign(request):
+    if request.method == "POST":
+        fm = SignUpForm(request.POST)
+        if fm.is_valid():
+            fm.save()
+            messages.success(request, 'Account Created Successfully !!')
+    else:
+        fm = SignUpForm()
+    return render(request, 'signup.html', {'form':fm})
+
+
+def logins(request):
+  if not request.user.is_authenticated:
+    if request.method == "POST":
+      fm = AuthenticationForm(request=request, data=request.POST)
+      if fm.is_valid():
+        uname = fm.cleaned_data['username']
+        upass = fm.cleaned_data['password']
+        user = authenticate(username=uname, password=upass)
+        if user is not None:
+          login(request, user)
+          messages.success(request, 'Logged in successfully !!')
+          return HttpResponseRedirect('/home/')
+    else:
+      fm = AuthenticationForm()
+    return render(request, 'login.html', {'form':fm})
+  else:
+    return HttpResponseRedirect('/additem/')
+
+
+def profile(request):
+  if request.user.is_authenticated:
+    return render(request, 'profile.html', {'name': request.user})
+  else:
+    return HttpResponseRedirect('/login/')
+
+
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/login/')
