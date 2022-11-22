8d3333bcbb712a5100f705e5d4bd695512e09df6
Bhagavaan <111573625+bhagavaan98@users.noreply.github.com>
2022-11-22 10:53:40 +0530

    mysql added

--- cargo/settings.py
+++ cargo/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'cargo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'quickport',
+        'USER':'root',
+        'HOST':'root',
+        'PASSWORD':'root',
+        'HOST':'localhost',
+        'PORT':3306
     }
 }
 
